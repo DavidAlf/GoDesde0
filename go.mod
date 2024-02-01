510172c588f72014c297df9f3ae5e5757ea702a6
David Alfonso <jdavid.alfonso18@gmail.com>
2024-02-01 11:11:23 -0500

    Primera version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DavidAlf/GoDesde0
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/DavidAlf/GoDesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
